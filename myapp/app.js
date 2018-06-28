0122d5d49a88c70ed5612cbb415dea5418342ab9
michellejjzhang <michellejiazhang@gmail.com>
2018-06-28 10:49:55 -0400

    we made it boys

--- myapp/app.js
+++ myapp/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,6 +38,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(3000);
+app.listen(8888);
 
 
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,34 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request'); // "Request" library
+var spotify_router = require('./spotify');
+var analysis = require('../services/spotify_analysis');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Michelle' });
-  res.send("hello");
+router.get('/', function(req, res) {
+    var loggedIn = false;
+    var access_token = req.cookies ? req.cookies['access_token'] : null;
+    console.log(req.cookies);
+    if (access_token){
+        var authOptions = {
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+                'Authorization': 'Bearer ' + access_token
+            },
+            json: true
+        };
+        request.get(authOptions, function(error, response) {
+            console.log(response.statusCode);
+            if (!error && response.statusCode === 200) {
+                console.log("reaching here");
+                loggedIn = true;
+            }
+            if (!loggedIn){
+                res.redirect('/spotify/');
+            } else {
+                res.redirect('/currently-playing/')
+            }
+        });
+    } else {
+        res.redirect('/spotify/');
+    }
 });
 
-const secureMiddlewares = {
-    msgAdder: function (req, res, next) {
-        req.mzhang = req.mzhang || {};
-        req.mzhang.msg = req.mzhang.msg || '';
-        req.mzhang.msg += 'secure';
-        next();
-    }
-};
+router.use('/spotify', spotify_router);
 
-router.use('/secure', secureMiddlewares.msgAdder);
-
-router.get('/secure/info',
-    function(req, res){
-        req.mzhang.msg += ' info';
-        res.send(req.mzhang.msg);
-    }
-);
-router.get('/secure/about',
-    function(req, res){
-        req.mzhang.msg += ' about';
-        res.send(req.mzhang.msg);
-    }
-);
+router.get('/currently-playing', function(req, res){
+    var access_token = req.cookies ? req.cookies['access_token'] : null;
+    analysis.findTrackName(access_token);
+    var data = analysis.returnDisplayData();
+    res.render('currently_playing', {data : data});
+});
 
 module.exports = router;
