882497595ad7448ddb4e62afce4bcfffd188b891
michellejjzhang <michellejiazhang@gmail.com>
2018-06-28 10:45:22 -0400

    commiting without clientid and client secret

---
+++ myapp/services/spotify_oauth.js
@@ -0,0 +1,112 @@
+var querystring = require('querystring');
+var request = require('request'); // "Request" library
+var analysis = require('./spotify_analysis');
+var cookie = require('cookie');
+
+var client_id = '';
+var client_secret = '';
+var redirect_uri = 'http://localhost:8888/spotify/callback'; 
+var stateKey = 'spotify_auth_state';
+
+var oauthorization = {
+    generateRandomString: function(length) {
+        var text = '';
+        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        for (var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    },
+    login: function(req, res) {
+        var state = oauthorization.generateRandomString(16);
+        res.cookie(stateKey, state);
+
+        var scope = 'user-read-private user-read-email user-read-currently-playing';
+        res.redirect('https://accounts.spotify.com/authorize?' +
+            querystring.stringify({
+                response_type: 'code',
+                client_id: client_id,
+                scope: scope,
+                redirect_uri: redirect_uri,
+                state: state
+            }));
+    },
+    getToken: function(req, res) {
+        // your application requests refresh and access tokens
+        // after checking the state parameter
+        var code = req.query.code || null;
+        var state = req.query.state || null;
+        var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+        if (state === null || state !== storedState) {
+            res.redirect('/#' +
+                querystring.stringify({
+                    error: 'state_mismatch'
+                }));
+        } else {
+            res.clearCookie(stateKey);
+            var authOptions = {
+                url: 'https://accounts.spotify.com/api/token',
+                form: {
+                    code: code,
+                    redirect_uri: redirect_uri,
+                    grant_type: 'authorization_code'
+                },
+                headers: {
+                    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+                },
+                json: true
+            };
+            request.post(authOptions, function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    oauthorization.setCookies(res, body);
+                    res.redirect('.././currently-playing');
+                } else {
+                    console.log("error");
+                    console.log(error);
+                    console.log(response.statusCode);
+                }
+            });
+        }
+    },
+    getNewToken: function(req, res) {
+        // requesting access token from refresh token
+        res.clearCookie('access_token');
+        var refresh_token = req.query.refresh_token;
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+            form: {
+                grant_type: 'refresh_token',
+                refresh_token: refresh_token
+            },
+            json: true
+        };
+
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var access_token = body.access_token;
+                oauthorization.setCookies(res, body);
+                res.send({
+                    'access_token': access_token
+                });
+            }
+        });
+    },
+    setCookies: function(res, body){
+        var set_cookies = [];
+        set_cookies.push(oauthorization.serializeCookie('access_token', body.access_token));
+        set_cookies.push(oauthorization.serializeCookie('refresh_token', body.refresh_token));
+        res.header("Set-Cookie", set_cookies);
+    },
+    serializeCookie: function(key, value) {
+        // This is res.cookie’s code without the array management and also ignores signed cookies.
+        if ('number' == typeof value) value = val.toString();
+        if ('object' == typeof value) value = JSON.stringify(val);
+        var path = {"path":"/"};
+        return cookie.serialize(key, value, path);
+    }
+};
+
+module.exports = oauthorization;
