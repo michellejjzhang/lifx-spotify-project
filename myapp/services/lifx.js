e73261125754480ff3964aac8894212a74105eb6
michellejjzhang <michellejiazhang@gmail.com>
2018-06-28 10:48:42 -0400

    commiting without bearer token

---
+++ myapp/services/lifx.js
@@ -0,0 +1,23 @@
+var lifx = require('lifx-http-api'),
+    client;
+
+client = new lifx({
+    bearerToken: ''
+});
+
+var control = {
+    setColor: function(color){
+        client.setState('all', {
+            power: 'on',
+            color: color,
+            duration: 1.5
+        }, function(err, data) {
+            if(err) {
+                console.error(err);
+                return;
+            }
+        });
+    }
+};
+
+module.exports = control;
